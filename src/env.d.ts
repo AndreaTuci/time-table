76f941e25e394a71a490c1855b1b7a0fe90937c6
Andrea Tuci <tuci.registrazioni@gmail.com>
2026-09-01 14:54:59 +0200

    build: vite, vue and typescript setup

---
+++ src/env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const componente: DefineComponent<{}, {}, any>
+  export default componente
+}
